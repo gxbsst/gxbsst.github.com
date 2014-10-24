b12576e2b92d879a4fe79e3861b1d4368a34234a
gxbsst <gxbsst@gmail.com>
2014-10-24 14:13:47 +0800

    new post

--- plugins/category_generator.rb
+++ plugins/category_generator.rb
@@ -106,9 +106,18 @@ module Jekyll
     # Loops through the list of category pages and processes each one.
     def write_category_indexes
       if self.layouts.key? 'category_index'
+        # dir = self.config['category_dir'] || 'categories'
+        # self.categories.keys.each do |category|
+        #   self.write_category_index(File.join(dir, category.to_url), category)
+        # end
+
         dir = self.config['category_dir'] || 'categories'
         self.categories.keys.each do |category|
-          self.write_category_index(File.join(dir, category.to_url), category)
+          cate_dir =  category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase
+          cate_dir = URI::escape(cate_dir)
+          cate_dir = URI::parse(cate_dir)
+          cate_dir = cate_dir.to_s
+          self.write_category_index(File.join(dir, cate_dir), category)
         end
 
       # Throw an exception if the layout couldn't be found.
--- plugins/category_list_tag.rb
+++ plugins/category_list_tag.rb
@@ -1,14 +1,25 @@
 # encoding: utf-8
 module Jekyll
   class CategoryListTag < Liquid::Tag
+    # def render(context)
+    #   html = ""
+    #   categories = context.registers[:site].categories.keys
+    #   categories.sort.each do |category|
+    #     posts_in_category = context.registers[:site].categories[category].size
+    #     category_dir = context.registers[:site].config['category_dir']
+    #     category_url = File.join(category_dir, category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase)
+    #     html << "<li class='category'><a href='/#{category_url}/'>#{category} (#{posts_in_category})</a></li>\n"
+    #   end
+    #   html
+    # end
     def render(context)
       html = ""
       categories = context.registers[:site].categories.keys
-      categories.sort.each do |category|
+      dir = context.registers[:site].config['category_dir']
+      categories.sort!.map do |category|
         posts_in_category = context.registers[:site].categories[category].size
-        category_dir = context.registers[:site].config['category_dir']
-        category_url = File.join(category_dir, category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase)
-        html << "<li class='category'><a href='/#{category_url}/'>#{category} (#{posts_in_category})</a></li>\n"
+        url =  dir+"/"+category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase
+        html << "<li class='category'><a href=' /#{url}/'>#{category} (#{posts_in_category})</a></li>\n"
       end
       html
     end
